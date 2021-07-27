8595052028da5648b8d619db09a64686fec53ba1
Suman Shrestha <sumanstha999@gmail.com>
2021-07-27 07:50:52 +0545

    blog using next js and tailwind css

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: "React Next Js Blog",
+    API_DEVELOPMENT: "http://localhost:8000/api/v1",
+    PRODUCTION: false,
+  },
+  images: {
+    domains: ["localhost", "139.59.53.101"],
+  },
+};
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      Hello World
+    </div>
+  );
+};
+
+export default Home;
